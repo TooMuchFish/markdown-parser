006f568a2f23faa21f922873fa3a38e6d1d8281d
TooMuchFish <103075501+TooMuchFish@users.noreply.github.com>
2022-05-05 17:32:53 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,36 +1,36 @@
 //https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class MarkdownParse {
+
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            if(!markdown.contains("[")) {
-                break; 
-            }
+            int exclaim = markdown.indexOf("!", currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
+            if (!(exclaim == -1) && ((exclaim + 1) == openBracket)) {
+                currentIndex = openBracket + 1;
+                continue;
+            }
+            if (openBracket == -1) break;
             int closeBracket = markdown.indexOf("]", openBracket);
+            if (closeBracket == -1) break;
             int openParen = markdown.indexOf("(", closeBracket);
+            if (openParen == -1) break;
             int closeParen = markdown.indexOf(")", openParen);
-            if (markdown.charAt(openBracket - 1) == '!') {
-                currentIndex = closeParen + 1;
-                continue; 
-            }
+            if (closeParen == -1) break;
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            if (closeParen < openBracket){
-                break;
-            }
             currentIndex = closeParen + 1;
         }
+
         return toReturn;
-    } 
-    
+    }
+
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
