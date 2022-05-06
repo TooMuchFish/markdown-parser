8d27963ff50b73a154668467b7244f3bee4a6e8f
TooMuchFish <103075501+TooMuchFish@users.noreply.github.com>
2022-05-05 17:29:39 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,38 +10,27 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        if(markdown.indexOf("(")!=-1 && markdown.indexOf("[")!=-1){
-            while(currentIndex < markdown.length()) {
-
-                if(markdown.indexOf("!")==0){
-                    break;
-                }
-
-                int nextOpenBracket = markdown.indexOf("[", currentIndex);
-                int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-                int openParen = markdown.indexOf("(", nextCloseBracket);
-                int closeParen = markdown.indexOf(")", openParen);
-
-                if(nextOpenBracket==-1||nextCloseBracket==-1){
-                    break;
-                }
-
-                if(openParen==-1||closeParen==-1){
-                    break;
-                }
-
-                if(openParen-nextCloseBracket==1){
-                    toReturn.add(markdown.substring(openParen + 1, closeParen));
-                }
-
-                currentIndex = closeParen + 1;
+        while(currentIndex < markdown.length()) {
+            if(!markdown.contains("[")) {
+                break; 
             }
-
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+            if (markdown.charAt(openBracket - 1) == '!') {
+                currentIndex = closeParen + 1;
+                continue; 
+            }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (closeParen < openBracket){
+                break;
+            }
+            currentIndex = closeParen + 1;
         }
-
         return toReturn;
-    }
-
+    } 
+    
 
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
